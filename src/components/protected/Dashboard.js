e822a4f8cdf4b93bda1e513f10070f2ff0aa4f64
Matthew Appleby <mappleby1@me.com>
2018-03-19 15:38:39 -0400

    started trying to subscribe to firestore updates

--- src/components/protected/Dashboard.js
+++ src/components/protected/Dashboard.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import { db } from '../../config/constants';
 
 export default class Dashboard extends Component {
+  state = {
+    openTabs: []
+  }
+
+  componentDidMount(){
+    db.collection("Tabs").get()
+    .then(snapshot => snapshot.forEach(doc => console.log('data: ', doc.data())))
+    // db.collection("Tabs").onSnapshot(function(snapshot) {
+    //   snapshot.docChanges.forEach(function(change) {
+    //       if (change.type === "added") {
+    //           console.log("New city: ", change.doc.data());
+    //       }
+    //       if (change.type === "modified") {
+    //           console.log("Modified city: ", change.doc.data());
+    //       }
+    //       if (change.type === "removed") {
+    //           console.log("Removed city: ", change.doc.data());
+    //       }
+    //   });
+  //});
+  }
+
   render() {
     return (
       <div>
