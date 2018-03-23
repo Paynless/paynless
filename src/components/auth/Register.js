d4843ffd319aba5bb8dd0dcea32f08824538c9de
Johnny <araujjohnny@gmail.com>
2018-03-23 11:08:39 -0400

    remove dob, consolidate form validation messaging

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -15,98 +15,56 @@ export default class Register extends Component {
       firstName: "",
       lastName: "",
       dob: "",
-      invalidEmailPw: false,
-      invalidName: false,
-      invalidDOB: false
     };
   }
 
   handleSubmit = event => {
     event.preventDefault();
     const { email, password, firstName, lastName, dob } = this.state;
-    if (!isValidEmail(email) || password.length < 4) {
-      this.setState(_ => ({
-        invalidEmailPw: true
-      }));
+    if (!isValidEmail(email) || password.length < 6) {
+      this.setState(_ => {
+        return setErrorMsg(Error('Email Must Be Valid And Password Must Be At Least 6 Characters'))
+      });
       return;
-    } else if (firstName.length < 3 || lastName.length < 3) {
-      this.setState(_ => ({
-        invalidName: true
-      }));
+    } else if (!firstName.length || !lastName.length) {
+      this.setState(_ => {
+        return setErrorMsg(Error('First And Last Name Cannot Be Left Blank'))
+      });
       return;
-    } else if (!isValidDOB(dob)) {
-      this.setState(_ => ({
-        invalidDOB: true
-      }));
-      return;
-    }
+    } 
+
     auth(email, password, firstName, lastName, dob).catch(err =>
       this.setState(setErrorMsg(err))
     );
   };
 
   render() {
-    const action = [
-      <FlatButton
-        label="Try Credientials Again"
-        primary={true}
-        onClick={evt =>
-          this.setState(_ => ({
-            invalidEmailPw: false
-          }))
-        }
-      />,
-      <FlatButton
-        label="Try Name Again"
-        primary={true}
-        onClick={evt =>
-          this.setState(_ => ({
-            invalidName: false
-          }))
-        }
-      />,
-      <FlatButton
-        label="Try DOB Again"
-        primary={true}
-        onClick={evt =>
-          this.setState(_ => ({
-            invalidDOB: false
-          }))
-        }
-      />
-    ];
     return (
       <form onSubmit={this.handleSubmit} style={style.container}>
         <h3>Register</h3>
         <TextField
           hintText="Enter your first name"
           floatingLabelText="First Name"
-          onChange={(event, newValue) => this.setState({ firstName: newValue })}
+          onChange={(event, newValue) => this.setState({ firstName: newValue, registerError: "", })}
         />
         <br />
         <TextField
           hintText="Enter your last name"
           floatingLabelText="Last Name"
-          onChange={(event, newValue) => this.setState({ lastName: newValue })}
-        />
-        <br />
-        <TextField
-          hintText="Enter your date of birth"
-          floatingLabelText="Date of Birth MM/DD/YYYY"
-          onChange={(event, newValue) => this.setState({ dob: newValue })}
+          onChange={(event, newValue) => this.setState({ lastName: newValue, registerError: "", })}
         />
         <br />
         <TextField
           hintText="Enter your Email"
           floatingLabelText="Email"
-          onChange={(event, newValue) => this.setState({ email: newValue })}
+          onChange={(event, newValue) => this.setState({ email: newValue, registerError: "" })}
         />
         <br />
         <TextField
           type="password"
           hintText="Enter your Password"
           floatingLabelText="Password"
-          onChange={(event, newValue) => this.setState({ password: newValue })}
+          onChange={(event, newValue) => this.setState({ password: newValue, registerError: "" })}
         />
         <br />
         {this.state.registerError && (
@@ -125,31 +83,6 @@ export default class Register extends Component {
           style={style.raisedBtn}
           type="submit"
         />
-        <Dialog
-          actions={action[0]}
-          modal={false}
-          open={this.state.invalidEmailPw}
-          onRequestClose={_ => this.setState(_ => ({ invalidEmailPw: false }))}
-        >
-          Please Enter A Valid Email Or Password
-        </Dialog>
-
-        <Dialog
-          actions={action[1]}
-          modal={false}
-          open={this.state.invalidName}
-          onRequestClose={evt => this.setState(_ => ({ invalidName: false }))}
-        >
-          Please Enter A Valid First And Last Name
-        </Dialog>
-        <Dialog
-          actions={action[2]}
-          modal={false}
-          open={this.state.invalidDOB}
-          onRequestClose={evt => this.setState(_ => ({ invalidDOB: false }))}
-        >
-          Please Enter A Valid Date Of Birth
-        </Dialog>
       </form>
     );
   }
