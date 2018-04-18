70376c5251dbac644a4f2c5970639174d1c982c7
Johnny <araujjohnny@gmail.com>
2018-04-18 09:46:15 -0400

    incorporate componentWillUpdate in to componentDidUpdate

--- src/components/main/LoadContext.js
+++ src/components/main/LoadContext.js
@@ -12,6 +12,7 @@ class LoadContext extends Component {
   };
 
   async componentDidMount() {
+    console.log(this.props.withAuth.user)
     try {
       const allOpenMerchants = await fetchAllMerchants();
       this.setState({ allOpenMerchants});
@@ -20,38 +21,58 @@ class LoadContext extends Component {
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    const { user: nextUser } = nextProps.withAuth
-    const { user: prevUser } = this.props.withAuth
+  // componentWillReceiveProps(nextProps) {
+  //   const { user: nextUser } = nextProps.withAuth
+  //   const { user: prevUser } = this.props.withAuth
     
-    if (nextUser === prevUser) return;
+  //   if (nextUser === prevUser) return;
 
-    this.removeUserListener && this.removeUserListener()
+  //   this.removeUserListener && this.removeUserListener()
 
-    if (!nextUser) return this.setState({ userObj: null });
+  //   if (!nextUser) return this.setState({ userObj: null });
     
-    this.removeUserListener = db
-      .collection("Users")
-      .doc(nextUser.uid)
-      .onSnapshot(snapshot => {
-        if (snapshot.exists) {
-          const userObj = snapshot.data();
-          this.setState({ userObj });
-        }
-      });
-  }
+  //   this.removeUserListener = db
+  //     .collection("Users")
+  //     .doc(nextUser.uid)
+  //     .onSnapshot(snapshot => {
+  //       if (snapshot.exists) {
+  //         const userObj = snapshot.data();
+  //         this.setState({ userObj });
+  //       }
+  //     });
+  // }
 
   componentDidUpdate(prevProps, prevState) {
     const { ready: thisAuthReady } = this.props.withAuth;
     const { allOpenMerchants: thisMerchants } = this.state;
-
-    if (thisAuthReady && thisMerchants.length > 0) {
-      const { ready: prevAuthReady } = prevProps.withAuth;
-      const { allOpenMerchants: prevMerchants } = prevState;
-
-      if (!prevAuthReady || prevMerchants.length === 0) {
+    const { allOpenMerchants: prevMerchants } = prevState;
+    console.log('component updated')
+    if (thisAuthReady) {
+      const { user: thisUser } = this.props.withAuth
+      const { user: prevUser } = prevProps.withAuth
+      console.log('merchants loaded?', !!thisMerchants.length)
+      if (thisMerchants.length > 0 && !prevMerchants.length) {
         this.setState({ isLoading: false });
       }
+      console.log('auth ready, userObj has changed?', thisUser !== prevUser)
+      if (thisUser === prevUser) return;
+      
+      console.log('userlistener exists?', !!this.removeUserListener)
+      this.removeUserListener && this.removeUserListener()
+      if (!thisUser) return this.setState({ userObj: null });
+
+      console.log('adding listener')
+      this.removeUserListener = db
+        .collection("Users")
+        .doc(thisUser.uid)
+        .onSnapshot(snapshot => {
+          if (snapshot.exists) {
+            console.log('user exists')
+            const userObj = snapshot.data();
+            this.setState({ userObj });
+          }
+        });
+      console.log('listener added?', this.state.userObj)
     }
   }
 
